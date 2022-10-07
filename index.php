a2ee14b655a25ced7eac611f9fef2e5adae38212
Bunyamin Aydemir <aydemir.buny@gmail.com>
2022-10-07 15:49:03 +0200

    Änderung Modell

--- index.php
+++ index.php
@@ -66,6 +66,7 @@
                                         <option>RS6</option>
                                         <option>RS6</option>
                                         <option>RS6</option>
+                                        <option>Modell S</option>
                                     </select>
                                 </div>
                             </div>
