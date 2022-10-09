a5dbf71ff0a892f862cda5f35e35b2cb43467cec
Bunyamin Aydemir <aydemir.buny@gmail.com>
2022-10-09 22:33:01 +0200

    Registrieren CSS

--- registrierung/registrierung.php
+++ registrierung/registrierung.php
@@ -6,25 +6,62 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrieren</title>
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet" href="registrierung.css">
     <script language="javascript" type="text/javascript" src="javascript.js"></script>
 </head>
 
 <body>
-
-    <h1>Registrieren</h1>
-
-    <div class="RegistrationForm">
-        <form>
-            <div>Vorname: <input type="text" name="eingabeVorname" /></div>
-            <div>Nachname: <input type="text" name="eingabeNachname" /></div>
-            <div>Benutzername: <input type="text" name="eingabeBenutzername" /></div>
-            <div>E-Mail: <input type="text" name="eingabeEmail" /></div>
-            <div>Passwort: <input type="password" name="eingabePasswort1" /></div>
-            <div>Passwort wiederholen: <input type="password" name="eingabePasswort2" /></div>
-            <button>Registrieren</button>
-        </form>
+<body>
+    <div class="containerBlur"></div>
+    <div class="container">
+        <div class="screen">
+            <div class="screen__content">
+                <h1>Registrieren</h1>
+                <form class="login">
+                    <div class="login__field__section">
+                        <div class="login__field">
+                            <i class="login__icon fas fa-user"></i>
+                            <input type="text" class="login__input" placeholder="Vorname">
+                        </div>
+                        <div class="login__field">
+                            <i class="login__icon fas fa-lock"></i>
+                            <input type="text" class="login__input" placeholder="Nachname">
+                        </div>
+                    </div>
+                    <div class="login__field__section">
+                        <div class="login__field login__field__plz">
+                            <i class="login__icon fas fa-user"></i>
+                            <input type="text" class="login__input login__input__plz" placeholder="PLZ">
+                        </div>
+                        <div class="login__field">
+                            <i class="login__icon fas fa-lock"></i>
+                            <input type="text" class="login__input" placeholder="Ort">
+                        </div>
+                        <div class="login__field">
+                            <i class="login__icon fas fa-lock"></i>
+                            <input type="text" class="login__input" placeholder="Straße und Hausnummer">
+                        </div>
+                    </div>
+                    <div class="login__field__section">
+                        <div class="login__field">
+                            <i class="login__icon fas fa-user"></i>
+                            <input type="text" class="login__input" placeholder="E-Mail">
+                        </div>
+                        <div class="login__field">
+                            <i class="login__icon fas fa-lock"></i>
+                            <input type="password" class="login__input" placeholder="Passwort">
+                        </div>
+                    </div>
+                    <button class="button login__submit">
+                        <span class="button__text">Registrieren</span>
+                        <i class="button__icon fas fa-chevron-right"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="screen__background">
+                <span class="screen__background__shape screen__background__shape1"></span>
+            </div>
+        </div>
     </div>
-
 </body>
 </html>
