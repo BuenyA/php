b86591c1e85f952e534a8dbfa3d746f1141c7626
Bunyamin Aydemir <aydemir.buny@gmail.com>
2022-10-09 18:35:32 +0200

    Anmeldung Design

--- Anmeldung/anmeldung.php
+++ Anmeldung/anmeldung.php
@@ -6,27 +6,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anmelden</title>
-    <link rel="stylesheet" href="stylesheet.css">
-    <script language="javascript" type="text/javascript" src="javascript.js"></script>
+    <link rel="stylesheet" href="anmeldung.css">
+    <!-- <script language="javascript" type="text/javascript" src="javascript.js"></script> -->
 </head>
-
 <body>
-
-    <h1>Anmelde-Seite</h1>
-    <div class="LogInForm">
-        <form>
-            <input type="text" name="eingabeBenutzername" placeholder="Benutzername"/>
-            <input type="password" name="eingabePasswort" placeholder="Passwort"/>
-            <button>Anmelden</button>
-        </form>
+    <div class="containerBlur"></div>
+    <div class="container">
+        <div class="screen">
+            <div class="screen__content">
+                <h1>Anmelden</h1>
+                <form class="login">
+                    <div class="login__field">
+                        <i class="login__icon fas fa-user"></i>
+                        <input type="text" class="login__input" placeholder="E-Mail">
+                    </div>
+                    <div class="login__field">
+                        <i class="login__icon fas fa-lock"></i>
+                        <input type="password" class="login__input" placeholder="Passwort">
+                    </div>
+                    <a href="">Passwort vergessen?</a>
+                    <button class="button login__submit">
+                        <span class="button__text">Anmelden</span>
+                        <i class="button__icon fas fa-chevron-right"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="screen__background">
+                <span class="screen__background__shape screen__background__shape1"></span>
+            </div>
+        </div>
     </div>
-
-
-    <?php
-        $Benutzername;
-        $Passwort;
-
-    ?>
-
 </body>
 </html>
