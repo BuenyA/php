d696c073dec0b3f232dca5fd7a8c24a7e279c1a6
Justin Janke <justin200307@gmail.com>
2022-10-10 13:03:42 +0200

    Anlegen Inserieren

---
+++ Inserieren/inserieren.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inserat aufgeben</title>
+    <link rel="stylesheet" href="inserieren.css">
+    <script language="javascript" type="module" src="inserieren.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
