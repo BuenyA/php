1ced824b1ed9f389f5ad277d526adf096e7202c8
Robin <Robin.schmied@gmail.com>
2022-10-10 09:22:19 +0200

    Passwort Reset Seite ergänzt

--- Anmeldung/passwordReset.php
+++ Anmeldung/passwordReset.php
@@ -14,20 +14,15 @@
     <div class="container">
         <div class="screen">
             <div class="screen__content">
-                <h1>Anmelden</h1>
+                <h1>Passwort <br /> Zurücksetzen</h1>
                 <form class="login">
                     <div class="login__field">
                         <i class="login__icon fas fa-user"></i>
                         <input type="text" class="login__input" placeholder="E-Mail">
                     </div>
-                    <div class="login__field">
-                        <i class="login__icon fas fa-lock"></i>
-                        <input type="password" class="login__input" placeholder="Passwort">
-                    </div>
-                    <a href="../registrierung/registrierung.php">Noch keinen Account?</a><br />
-                    <a href="../anmeldung/passwordReset.php">Passwort vergessen?</a>
-                    <button class="button login__submit">
-                        <span class="button__text">Anmelden</span>
+                    <a href="../Anmeldung/anmeldung.php">Zurück zum Login</a><br /><br/>
+                    <button class="button resetPw__submit">
+                        <span class="button__text">Code anfordern</span>
                         <i class="button__icon fas fa-chevron-right"></i>
                     </button>
                 </form>
