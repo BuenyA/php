4e0ea33cbb377edac56fcce07b0b1ee8e3e2f3c6
Bunyamin Aydemir <aydemir.buny@gmail.com>
2022-10-12 10:55:29 +0200

    navigationBar LastMinute

--- LastMinute/lastminute.php
+++ LastMinute/lastminute.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html lang="de">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autostar - Last Minute Angebote</title>
+    <link rel="icon" type="image/png" href="image/AutostarLogoIconTab.png" wid>
+    <link rel="stylesheet" href="lastminute.css">
+    <!-- <script language="javascript" type="text/javascript" src="javascript.js"></script> -->
+</head>
+
+<body>
+    <div class="backgroundImageFilter">
+        <div class="navigationMenu">
+            <div class="navigationMenuLogo">
+                <img src="image/AutostarLogo.png" width="200" height="40" onclick="window.location.href = '../index.php'">
+            </div>
+            <ul class="navigationElements">
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../TopAngebote/topangebote.php">Top Angebote</a></li>
+                <li><a href="">Last-Minute</a></li>
+                <li><a href="../inserieren/inserieren.php">Verkaufen</a></li>
+            </ul>
+            <div class="navigationMenuButton">
+                <div class="navigationMenuButtonAnmelden">
+                    <button onclick="window.location.href = '../anmeldung/anmeldung.php'"><b>Anmelden</b></button>
+                </div>
+                <div class="navigationMenuButtonRegistrieren">
+                    <button onclick="window.location.href = '../registrierung/registrierung.php'"><b>Registrieren</b></button>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
