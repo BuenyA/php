1f7fc05a858936a3a082f462435528356808dd2a
Bünyamin Aydemir <aydemir.buny@gmail.com>
2022-10-26 20:57:54 +0200

    PHP

--- db.php
+++ db.php
@@ -1,12 +1,9 @@
 <?php
-
-//Stellt eine Datenbankverbindung mit der Datenbank "Autostar" her
-
-$dsn = 'mysql:dbname=Autostar;host=db;port=3306';
-
-try {
-    $db = new PDO( $dsn, 'root', '' );
-} catch (PDOException $e){
-    exit( 'Connection failed: ' . $e->getMessage());
-}
+    //Stellt eine Datenbankverbindung mit der Datenbank "Autostar" her
+    $dsn = 'mysql:dbname=Autostar;host=db;port=3306';
+    try {
+        $db = new PDO( $dsn, 'root', '' );
+    } catch (PDOException $e){
+        exit( 'Connection failed: ' . $e->getMessage());
+    }
 ?>
