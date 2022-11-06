ec1646b643dac478030c9025df3676f239486b38
Bunyamin Aydemir <aydemir.buny@gmail.com>
2022-11-06 21:27:25 +0100

    SQL-Anpassung

--- index.php
+++ index.php
@@ -132,7 +132,7 @@
     <section class="topOffer">
         <h1>Top-Angebote</h1>
         <?php
-            $queryInserat = "SELECT * FROM Inserat JOIN Accounts ON Inserat.Inhaber_Nr = Accounts.account_ID ORDER BY Erstzulassung ASC";
+            $queryInserat = "SELECT * FROM Inserat JOIN Accounts ON Inserat.Inhaber_Nr = Accounts.account_ID WHERE Auktionsende >= CURRENT_TIMESTAMP ORDER BY Preis ASC";
             $resInserat = $db->query($queryInserat);
 
             if (empty($_SESSION['user'])) {
@@ -149,7 +149,7 @@
     <section class="topOffer">
         <h1>Last-Minute-Angebote...</h1>
         <?php
-            $queryInserat = "SELECT * FROM Inserat JOIN Accounts ON Inserat.Inhaber_Nr = Accounts.account_ID ORDER BY Erstzulassung ASC";
+            $queryInserat = "SELECT * FROM Inserat JOIN Accounts ON Inserat.Inhaber_Nr = Accounts.account_ID WHERE Auktionsende >= CURRENT_TIMESTAMP ORDER BY Auktionsende ASC";
             $resInserat = $db->query($queryInserat);
             if (empty($_SESSION['user'])) {
                 phpFunctions::showOffer(7, $resInserat, 0, 4);
