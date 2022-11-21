effbc76121fdba3fdc7e476a7314a6c52418ccfe
Bunyamin Aydemir <aydemir.buny@gmail.com>
2022-11-21 22:48:55 +0100

    Inseratbestätigung

---
+++ Inserieren/erfolgreichInseriert.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vielen Dank für Ihre Registrierung!</title>
+    <link rel="stylesheet" href="produkt.css">
+    <script language="javascript" type="text/javascript" src="../index.js"></script>
+    <link rel="stylesheet" href="./erfolgreichInseriert.css">
+</head>
+<body>
+    <div>
+        <img class="haken" src="../image/prufen.png" width="100" heigth="100">
+        <h1>Vielen Dank für Ihre Auktion!</h1>
+        <h2>Ihre Auktion wird in den nächsten Minuten angelegt</h2>
+        <button onClick="window.location = '../index.php'">Zur Homepage</button>
+    </div>
+</body>
+</html>
